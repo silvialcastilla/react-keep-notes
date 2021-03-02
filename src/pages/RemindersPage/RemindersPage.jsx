7d5d496b1185a340a54813f265305ad3b6935778
silvialcastilla <silope0302@gmail.com>
2021-03-02 18:32:39 +0100

    pie chartist

--- src/pages/RemindersPage/RemindersPage.jsx
+++ src/pages/RemindersPage/RemindersPage.jsx
@@ -1,37 +1,79 @@
-import React from 'react'
-import Aside from '../../components/Aside/Aside'
+import React from "react";
+import Aside from "../../components/Aside/Aside";
 //import Logo from "../../components/logo/Logo";
-import ReactDOM from 'react-dom';
-import ChartistGraph from 'react-chartist';
-
-
+import ReactDOM from "react-dom";
+import ChartistGraph from "react-chartist";
 
 function RemindersPage() {
+  let jsonPrueba = [
+    {
+      labels: ["json", "Ahhhhhhhh", "Cableeee"],
+      series: [10, 30, 60],
+    },
+    {
+      labels: ["json", "Ahhhhhhhh", "Cableeee"],
+      series: [
+        [5, 4, 3, 7, 5, 10, 3, 4, 8, 10, 6, 8],
+        [3, 2, 9, 5, 4, 6, 4, 6, 7, 8, 7, 4],
+      ],
+    },
+    {
+      labels: ["json", "Ahhhhhhhh", "Cableeee"],
+      series: [
+        [12, 9, 7, 8, 5],
+        [2, 1, 3.5, 7, 3],
+        [1, 3, 4, 5, 6],
+      ],
+    },
+  ];
+
+  console.log(jsonPrueba[0].labels);
+
   var data = {
-    labels: ['W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7', 'W8', 'W9', 'W10'],
-    series: [
-      [1, 2, 4, 8, 6, -2, -1, -4, -6, -2]
-    ]
+    labels: jsonPrueba[0].labels,
+    series: jsonPrueba[0].series,
   };
 
   var options = {
-    high: 10,
-    low: -10,
-    axisX: {
-      labelInterpolationFnc: function(value, index) {
-        return index % 2 === 0 ? value : null;
-      }
-    }
+    labelInterpolationFnc: function (value) {
+      return value[0];
+    },
   };
 
-  var type = 'Bar'
+  var responsiveOptions = [
+    [
+      "screen and (min-width: 640px)",
+      {
+        chartPadding: 30,
+        labelOffset: 100,
+        labelDirection: "explode",
+        labelInterpolationFnc: function (value) {
+          return value;
+        },
+      },
+    ],
+    [
+      "screen and (min-width: 1024px)",
+      {
+        labelOffset: 80,
+        chartPadding: 20,
+      },
+    ],
+  ];
 
+  var type = "Pie";
 
   return (
     <div className="reminders-page">
       <h1>RemindersPage</h1>
-      <Aside/>
-      <ChartistGraph className={'ct-octave'} data={data} options={options} type={type} />
+      <Aside />
+      <ChartistGraph
+        className={"ct-chart"}
+        data={data}
+        options={options}
+        responsiveOptions={responsiveOptions}
+        type={type}
+      />
     </div>
   );
 }
